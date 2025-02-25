01c3426744240a25efad3b00ad384748c46864a2
MrPuppy3000 <ryanproce@gmail.com>
2025-02-24 23:43:43 -0500

    Bishop rechanged

--- chess/Bishop.java
+++ chess/Bishop.java
@@ -25,50 +25,58 @@ public class Bishop extends Piece {
                 switch ((rankDifference > 0 ? 2 : 0) + (fileDifference > 0 ? 1 : 0)) {
                     case 3: //Down left
                         while (rank != nextRank)
+                        file = Piece.prev(file);
+                        rank--;
                         {
-                            file = Piece.prev(file);
-                            rank--;
-
                             if (!isSquareEmpty(file, rank))
                             {
                                 return false;
                             }
+
+                            file = Piece.prev(file);
+                            rank--;
                         }
                         break;
                     case 2: //Down right
+                        file = Piece.next(file);
+                        rank--;
                         while (rank != nextRank)
                         {
-                            file = Piece.next(file);
-                            rank--;
-
                             if (!isSquareEmpty(file, rank))
                             {
                                 return false;
                             }
+
+                            file = Piece.next(file);
+                            rank--;
                         }
                         break;
                     case 1: //Up left
                         while (rank != nextRank)
+                        file = Piece.prev(file);
+                        rank++;
                         {
-                            file = Piece.prev(file);
-                            rank++;
-
                             if (!isSquareEmpty(file, rank))
                             {
                                 return false;
                             }
+
+                            file = Piece.prev(file);
+                            rank++;
                         }
                         break;
                     case 0: //Up right
                         while (rank != nextRank)
+                        file = Piece.next(file);
+                        rank++;
                         {
-                            file = Piece.next(file);
-                            rank++;
-
                             if (!isSquareEmpty(file, rank))
                             {
                                 return false;
                             }
+
+                            file = Piece.next(file);
+                            rank++;
                         }
                         break;
                 }
